fbc6a9a90e7d5b581660a22d7de26ed566c6d946
Ruslan N <webmaster.ruslann@gmail.com>
2017-12-30 07:44:01 -0700

    String merger
    Custom function for strings merging with any type of punctuation.

---
+++ stringMerger.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Merge strings with any type of punctuation or ampersand
+ * @param $strings
+ * @param bool $amp
+ * @param string $punctuation
+ * @return string
+ */
+
+function merge_strings($strings, $amp = false, $punctuation = ', ')
+{
+    $result = null;
+    $i = 1;
+    $length = -strlen($punctuation); // negative integer
+    $myString = count($strings);
+
+    foreach($strings as $string) {
+        if($myString > 2 && $amp === true) {
+            if($i < 2)
+                $result .= $string. ' &amp; ';
+            else
+                $result .= $string.$punctuation;
+            $i++;
+        }
+        elseif ($myString == 2 && $amp === true) {
+            $result .= $string. ' &amp; ';
+            $length = -7;
+        }
+        elseif ($myString > 2 && $amp === false)
+            $result .= $string.$punctuation;
+        elseif ($myString == 2 && $amp === false)
+            $result .= $string.$punctuation;
+        elseif ($myString < 2)
+            $result .= $string.$punctuation;
+        else
+            $result .= $string.$punctuation;
+    }
+
+    // Return final string without last punctuation
+    return substr($result, 0, $length);
+}
+
+    $strs = ['Ruslan', 'Ksu', 'Kathy', 'Fistash', 'Moshkina', 'Areshkina'];
+echo merge_strings($strs, true);
+
